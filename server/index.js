98fe915013530f11259ed204a5deefeaf125dd1d
rana <rana.ahammed.012@gmail.com>
2023-05-02 12:52:07 +0600

    3

--- server/index.js
+++ server/index.js
@@ -16,10 +16,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", Route);
 Connection();
 
+const __dirname1 = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname1, "../client/build")));
 	app.get("/", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+		res.sendFile(path.join(__dirname1, "../client/build/index.html"));
 	});
 }
 
